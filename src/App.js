20da597474dd4ef9ad6f8f44bf8d5b25ea726498
serpil safa <serpilsafa@gmail.com>
2020-11-03 09:14:58 +0000

    togglebutton

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ state = {
     {name : 'Seda', age : 25},
     {name : 'Sedef', age : 21}
   ],
-  otherState: 'Some text here'
+  otherState: 'Some text here',
+  showPersons: false
  };
 
  clickedButton = (event) => {
@@ -24,22 +25,31 @@ state = {
   });
 };
 
+togglePersonHandler = () =>{
+  const doesShow = this.state.showPersons;
+  this.setState({showPersons: !doesShow});
+
+}
 
  render(){
   return (
     <div className="App">
       <h1>Hello React!</h1>
-      <button onClick={this.clickedButton}>Click button</button>
-      <Person 
-        name = {this.state.persons[0].name} 
-        age = {this.state.persons[0].age}
-        changed = {this.clickedButton} />
-      <Person 
-        name = {this.state.persons[1].name} 
-        age = {this.state.persons[1].age} >She is my sister</Person>
-      <Person 
-        name = {this.state.persons[2].name} 
-        age = {this.state.persons[2].age} />
+      <button onClick={this.togglePersonHandler}>Toggle Button</button>
+      { this.state.showPersons ?
+        <div>
+        <Person 
+          name = {this.state.persons[0].name} 
+          age = {this.state.persons[0].age}
+          changed = {this.clickedButton} />
+        <Person 
+          name = {this.state.persons[1].name} 
+          age = {this.state.persons[1].age} >She is my sister</Person>
+        <Person 
+          name = {this.state.persons[2].name} 
+          age = {this.state.persons[2].age} />
+      </div>:null
+      }
     </div>
   );
  }
